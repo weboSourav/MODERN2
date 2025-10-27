afec3e63156105d3bc8c18ba16b48ab4dec3425d
unknown <winterfell118@gmail.com>
2025-10-27 22:53:42 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState } from "react";
+import OverylayMenu from "./OverylayMenu";
 
-const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+
+
+
+export default function NavBar(){
+const [menuOpen,setMenuOpen] = useState(false);
+const [visible,setVisible] = useState(true);
+
+
+
+return(
+<>
+<nav className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? 'translate-y-0': '-translate-y-full'}`}>
+
+
+</nav>
+
+
+
+
+
+
+
+
+
+
+<OverylayMenu/>
+
+</>
+
+)
 }
-
-export default NavBar
